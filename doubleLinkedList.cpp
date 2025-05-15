5f2a0eae2b4295fdaa1328e9e1233859eccaf22a
Hani-hue <isnahabibah8729@gmail.com>
2025-05-15 11:08:59 +0700

    start.prev = newNode (if start exists)

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -43,3 +43,7 @@ public:
             }
             // step 4 : newNode.next = START
             newNode->next = START;
+
+            // step 5 : start.prev = newNode (if start exists)
+            if (START != NULL)
+                START->prev = newNode;
