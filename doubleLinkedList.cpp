c8d68b381785195897c63426605312dacc7caf43
Hani-hue <isnahabibah8729@gmail.com>
2025-05-15 13:04:56 +0700

    int main

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -191,6 +191,38 @@ public:
         else
         {
             cout << "Record found\n";
-            cout << "Roll Number: " << current->noMhs << endl;
+            cout << "Roll Number: " << current->noMhs << endl;
         }
-    }
+
+        int main()
+
+            DoubleLinkedList list;
+        char choice;
+
+        do
+        {
+            cout << "\nMenu\n";
+            cout << "1. Add Record\n";
+            cout << "2. Delete Record\n";
+            cout << "3. View Ascending\n";
+            cout << "4. View Descending\n";
+            cout << "5. Search Record\n";
+            cout << "6. Exit\n";
+            cout << "Enter your choice: ";
+
+            cin >> choice;
+        }
+        cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        }
